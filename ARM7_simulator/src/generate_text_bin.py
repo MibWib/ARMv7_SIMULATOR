d4e735c7507b1ad3238b6eb7622ad2ef0ea482f4
Aarush-04 <pahuja.aarush.ap@gmail.com>
2025-06-26 00:40:37 -0400

    updated generate_text_bin  slightly new test_custom bin file. everything seems to be working fine.

--- ARM7_simulator/src/generate_text_bin.py
+++ ARM7_simulator/src/generate_text_bin.py
@@ -1,4 +1,8 @@
 with open("test_custom.bin", "wb") as f:
-    f.write(bytes.fromhex("E3A00001"))  # MOV R0, #1
-    f.write(bytes.fromhex("E2801002"))  # ADD R1, R0, #2
-    f.write(bytes.fromhex("EAFFFFFE"))  # B . (infinite loop)
+    f.write(bytes.fromhex("01 00 A0 E3"))  # MOV R0, #1
+    f.write(bytes.fromhex("02 10 80 E2"))  # ADD R1, R0, #2
+    f.write(bytes.fromhex("05 20 A0 E3"))  # MOV R2, #5
+    f.write(bytes.fromhex("02 30 42 E2"))  # SUB R3, R2, #2
+    f.write(bytes.fromhex("03 30 53 E3"))  # CMP R3, #3
+    f.write(bytes.fromhex("00 40 83 E0"))  # ADD R4, R3, R0
+    f.write(bytes.fromhex("FE FF FF EA"))  # B . (infinite loop)
