f56b1c0ae88644199d500d5fc6f3f6506c2ed33b
Aarush-04 <pahuja.aarush.ap@gmail.com>
2025-06-26 12:36:07 -0400

    new bin file test_straightline is 21 lines
    this one works correctly as well so should be the main testing file.

--- ARM7_simulator/src/generate_text_bin.py
+++ ARM7_simulator/src/generate_text_bin.py
@@ -1,8 +1,23 @@
-with open("test_custom.bin", "wb") as f:
+with open("test_straightline_with_cmp.bin", "wb") as f:
     f.write(bytes.fromhex("01 00 A0 E3"))  # MOV R0, #1
-    f.write(bytes.fromhex("02 10 80 E2"))  # ADD R1, R0, #2
-    f.write(bytes.fromhex("05 20 A0 E3"))  # MOV R2, #5
-    f.write(bytes.fromhex("02 30 42 E2"))  # SUB R3, R2, #2
-    f.write(bytes.fromhex("03 30 53 E3"))  # CMP R3, #3
-    f.write(bytes.fromhex("00 40 83 E0"))  # ADD R4, R3, R0
-    f.write(bytes.fromhex("FE FF FF EA"))  # B . (infinite loop)
+    f.write(bytes.fromhex("02 10 A0 E3"))  # MOV R1, #2
+    f.write(bytes.fromhex("03 20 A0 E3"))  # MOV R2, #3
+    f.write(bytes.fromhex("01 30 80 E0"))  # ADD R3, R0, R1
+    f.write(bytes.fromhex("02 30 83 E0"))  # ADD R3, R3, R2
+    f.write(bytes.fromhex("04 40 A0 E3"))  # MOV R4, #4
+    f.write(bytes.fromhex("05 50 A0 E3"))  # MOV R5, #5
+    f.write(bytes.fromhex("06 60 A0 E3"))  # MOV R6, #6
+    f.write(bytes.fromhex("04 70 84 E0"))  # ADD R7, R4, R4
+    f.write(bytes.fromhex("05 70 87 E0"))  # ADD R7, R7, R5
+    f.write(bytes.fromhex("06 70 C7 E0"))  # SUB R7, R7, R6
+    f.write(bytes.fromhex("07 30 53 E1"))  # CMP R3, R7
+    f.write(bytes.fromhex("08 80 A0 E3"))  # MOV R8, #8
+    f.write(bytes.fromhex("09 90 A0 E3"))  # MOV R9, #9
+    f.write(bytes.fromhex("0A A0 A0 E3"))  # MOV R10, #10
+    f.write(bytes.fromhex("0B B0 A0 E3"))  # MOV R11, #11
+    f.write(bytes.fromhex("07 C0 83 E1"))  # ORR R12, R3, R7
+    f.write(bytes.fromhex("08 C0 C8 E2"))  # EOR R12, R12, R8
+    f.write(bytes.fromhex("09 C0 0C E0"))  # AND R12, R12, R9
+    f.write(bytes.fromhex("0D D0 A0 E1"))  # MOV R13, R13
+    f.write(bytes.fromhex("0E E0 A0 E1"))  # MOV R14, R14
+    f.write(bytes.fromhex("FE FF FF EA"))  # B .
